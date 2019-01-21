e210b4a1651b7a0dcce402a44fcc3852fbbd76e6
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2019-01-21 11:45:12 +0500

    added basic routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
@@ -12,7 +13,14 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        {/* this is not inside Switch since its gonna be displayed on all pages */}
         <NavBar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
       </React.Fragment>
     );
   }
