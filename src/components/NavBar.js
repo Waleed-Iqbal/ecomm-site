901982f2ab7f9a30a19281ca7ee21cf2ccd6ac4e
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-05-07 05:26:37 +0500

    using styled components for the car button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import { ButtonContainer } from "./Button";
 
 export default class NavBar extends Component {
   render() {
@@ -21,10 +22,12 @@ export default class NavBar extends Component {
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus" />
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
             My Cart
-          </button>
+          </ButtonContainer>
         </Link>
       </nav>
     );
