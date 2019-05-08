8b49ad4bbf11b86bf4061dcaecc186db893eaccc
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-05-08 23:07:49 +0500

    Product component improved

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import { styled } from "styled-components";
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from "../context";
 
 export default class Product extends Component {
   render() {
+    const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
+      <ProductWrapper class="col-9 mx-auto my-3 col-md-6 col-lg-3">
         <h3>Hello from Product</h3>
-      </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div`
+`
