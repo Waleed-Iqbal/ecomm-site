ee9233c72b8d67f0aadbe710ea546831d341f8d4
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-05-12 14:22:37 +0500

    initial setup for the cart

--- src/context.js
+++ src/context.js
@@ -9,7 +9,10 @@ class ProductProvider extends Component {
     detailProduct: detailProduct,
     cart: [],
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
 
   setProducts = () => {
@@ -42,7 +45,7 @@ class ProductProvider extends Component {
     product.count = 1;
     const price = product.price;
     product.total = price;
-    
+
     this.setState({
       products: allProducts,
       cart: [...this.state.cart, product]
@@ -62,6 +65,22 @@ class ProductProvider extends Component {
     });
   }
 
+  increment = (id) => {
+    console.log("increment");
+  }
+
+  decrement = (id) => {
+    console.log("decrement");
+  }
+
+  removeItem = (id) => {
+    console.log("remove from cart");
+  }
+
+  clearCart = () => {
+    console.log("cart cleared");
+  }
+
   render() {
     return (
       <ProductContext.Provider value={{
@@ -69,7 +88,11 @@ class ProductProvider extends Component {
         handleDetail: this.handleDetail,
         addToCart: this.addToCart,
         openModal: this.openModal,
-        closeModal: this.closeModal
+        closeModal: this.closeModal,
+        increment: this.increment,
+        decrement: this.decrement,
+        removeItem: this.removeItem,
+        clearCart: this.clearCart
       }}>
         {this.props.children}
       </ProductContext.Provider>
